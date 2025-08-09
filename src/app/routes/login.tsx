3bae2e9c0cd8904f3c1029d3198a0a496efe15ea
Ryan Bezold <Ryan.Bezold@gmail.com>
2025-08-09 14:06:40 -0500

    feat: Add login page

--- src/app/routes/login.tsx
+++ src/app/routes/login.tsx
@@ -1,3 +1,60 @@
-export default function Login(){
-    return("Login placeholder");
+import type { Route } from "./+types/login";
+
+import TextField from "@/components/ui/TextField";
+import { toaster } from "@/components/ui/toaster";
+import { authenticationAxiosInstance } from "@/shared/api/axios";
+import { Button, Card, Center, Spinner } from "@chakra-ui/react";
+import { AxiosError } from "axios";
+import { Link, redirect, useFetcher } from "react-router";
+
+export async function clientAction({ request }: Route.ClientActionArgs) {
+  const response = await authenticationAxiosInstance
+    .postForm("/login", request.formData)
+    .catch((error: AxiosError) => {
+      // TODO: Should extract this logic into a response handler.
+      if (error.response?.status === 401) {
+        toaster.error({ description: "Invalid email or password" });
+      } else if (error.response?.status === 500) {
+        toaster.error({ description: "Server error, please try again later" });
+      } else {
+        toaster.error({ description: "An unexpected error occurred" });
+      }
+      console.error("Login error:", error);
+      return null;
+    });
+  redirect("/");
+  return null;
+}
+
+export default function Login() {
+  const fetcher = useFetcher();
+
+  return (
+    <Center height={"100%"}>
+      <Card.Root>
+        <Card.Header>
+          <Card.Title textAlign={"center"}>Login</Card.Title>
+        </Card.Header>
+        <fetcher.Form method="post">
+          <Card.Body>
+            <TextField label="Email" name="email" type="email" required />
+            <TextField
+              label="Password"
+              name="password"
+              type="password"
+              required
+            />
+          </Card.Body>
+          <Card.Footer justifyContent={"end"}>
+            <Button variant={"outline"} asChild>
+              <Link to="/sign-up">Sign Up</Link>
+            </Button>
+            <Button type="submit">
+              {fetcher.state !== "idle" ? <Spinner /> : "Login"}
+            </Button>
+          </Card.Footer>
+        </fetcher.Form>
+      </Card.Root>
+    </Center>
+  );
 }
--- src/shared/api/axios.ts
+++ src/shared/api/axios.ts
@@ -5,3 +5,7 @@ import {config} from "@/shared";
 export const axiosInstance = axios.create({
     baseURL: config.backendUrl,
 })
+
+export const authenticationAxiosInstance = axios.create({
+    baseURL: config.authenticationUrl,
+})
