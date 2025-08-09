362cd0928521673339af22cda7ec05178e6e7efb
Ryan Bezold <ryan.bezold@gmail.com>
2025-08-09 11:18:45 -0500

    fix: Move axios error handling into thenable

--- src/entities/book/api/fetchBookById.ts
+++ src/entities/book/api/fetchBookById.ts
@@ -1,18 +1,21 @@
 import type { BookModel } from "@/entities/book";
 import {axiosInstance} from "@/shared";
+import type {AxiosError} from "axios";
 
 export async function fetchBookById(bookId: string): Promise<BookModel> {
   console.log("Fetching book by ID from backend...");
   try {
       return await axiosInstance.get(`/books/${bookId}`)
-          .then(response => response.data);
+          .then(response => response.data)
+          .catch((error: AxiosError) => {
+              // Check for Axios 404 error
+              if (error && error.response && error.response.status === 404) {
+                  const message = `Book not found (ID: ${bookId})`;
+                  console.error(message);
+                  throw new Error(message);
+              }
+          });
   } catch (error) {
-    // Check for Axios 404 error
-    if (error && error.response && error.response.status === 404) {
-      const message = `Book not found (ID: ${bookId})`;
-      console.error(message);
-      throw new Error(message);
-    }
     console.error('Failed to fetch books:', error);
     throw error; // Re-throw the error for further handling
   }
