ff118308e50180d6849a5739f247da1462c00301
Ryan Bezold <ryan.bezold@gmail.com>
2025-08-10 07:26:32 -0500

    feat: Implement login handler

--- src/app/routes/login.tsx
+++ src/app/routes/login.tsx
@@ -1,60 +1,50 @@
-import type { Route } from "./+types/login";
+import type {Route} from "./+types/login";
 
 import TextField from "@/components/ui/TextField";
-import { toaster } from "@/components/ui/toaster";
-import { authenticationAxiosInstance } from "@/shared/api/axios";
-import { Button, Card, Center, Spinner } from "@chakra-ui/react";
-import { AxiosError } from "axios";
-import { Link, redirect, useFetcher } from "react-router";
+import {Button, Card, Center, Spinner} from "@chakra-ui/react";
+import {Link, redirect, useFetcher} from "react-router";
+import {toaster} from "@/components/ui/toaster";
+import {login} from "@/features/authentication/api/login";
 
-export async function clientAction({ request }: Route.ClientActionArgs) {
-  const response = await authenticationAxiosInstance
-    .postForm("/login", request.formData)
-    .catch((error: AxiosError) => {
-      // TODO: Should extract this logic into a response handler.
-      if (error.response?.status === 401) {
-        toaster.error({ description: "Invalid email or password" });
-      } else if (error.response?.status === 500) {
-        toaster.error({ description: "Server error, please try again later" });
-      } else {
-        toaster.error({ description: "An unexpected error occurred" });
-      }
-      console.error("Login error:", error);
-      return null;
-    });
-  redirect("/");
-  return null;
+export async function clientAction({request}: Route.ClientActionArgs) {
+    try {
+        const formData = await request.formData();
+        await login(formData);
+    } catch (error) {
+        console.error(error);
+        toaster.error({description: "Something went wrong, please try again later"});
+    }
 }
 
 export default function Login() {
-  const fetcher = useFetcher();
+    const fetcher = useFetcher();
 
-  return (
-    <Center height={"100%"}>
-      <Card.Root>
-        <Card.Header>
-          <Card.Title textAlign={"center"}>Login</Card.Title>
-        </Card.Header>
-        <fetcher.Form method="post">
-          <Card.Body>
-            <TextField label="Email" name="email" type="email" required />
-            <TextField
-              label="Password"
-              name="password"
-              type="password"
-              required
-            />
-          </Card.Body>
-          <Card.Footer justifyContent={"end"}>
-            <Button variant={"outline"} asChild>
-              <Link to="/sign-up">Sign Up</Link>
-            </Button>
-            <Button type="submit">
-              {fetcher.state !== "idle" ? <Spinner /> : "Login"}
-            </Button>
-          </Card.Footer>
-        </fetcher.Form>
-      </Card.Root>
-    </Center>
-  );
+    return (
+        <Center height={"100%"}>
+            <Card.Root>
+                <Card.Header>
+                    <Card.Title textAlign={"center"}>Login</Card.Title>
+                </Card.Header>
+                <fetcher.Form method="post">
+                    <Card.Body>
+                        <TextField label="Email" name="email" type="email" required/>
+                        <TextField
+                            label="Password"
+                            name="password"
+                            type="password"
+                            required
+                        />
+                    </Card.Body>
+                    <Card.Footer justifyContent={"end"}>
+                        <Button variant={"outline"} asChild>
+                            <Link to="/sign-up">Sign Up</Link>
+                        </Button>
+                        <Button type="submit">
+                            {fetcher.state !== "idle" ? <Spinner/> : "Login"}
+                        </Button>
+                    </Card.Footer>
+                </fetcher.Form>
+            </Card.Root>
+        </Center>
+    );
 }
---
+++ src/features/authentication/api/login.ts
@@ -0,0 +1,14 @@
+import {LoginDto} from "@/features/authentication/dtos";
+import {authenticationAxiosInstance, axiosErrorHandler} from "@/features/authentication/api/axios";
+import {redirect} from "react-router";
+
+export async function login(formData: FormData) {
+    const email = formData.get('email') as string;
+    const password = formData.get('password') as string;
+
+    const loginDto = new LoginDto(email, password);
+
+    await authenticationAxiosInstance.post("/login", loginDto).catch(axiosErrorHandler);
+
+    redirect("/");
+}
