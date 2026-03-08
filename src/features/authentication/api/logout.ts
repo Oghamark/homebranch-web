dd7a6014261ac4c3e96660392ae1a40b4572a244
Ryan Bezold <ryan.bezold@gmail.com>
2026-03-08 19:56:14 -0400

    fix: Invalidate refresh tokens on logout

--- src/features/authentication/api/logout.ts
+++ src/features/authentication/api/logout.ts
@@ -4,7 +4,7 @@ import {store} from "@/app/store";
 import {homebranchApi} from "@/shared/api/rtk-query";
 
 export async function logout() {
-    return await authenticationAxiosInstance.post("/logout").then(response => {
+    return await authenticationAxiosInstance.post("/refresh/invalidate").then(response => {
         sessionStorage.removeItem('user_id');
         store.dispatch(homebranchApi.util.resetApiState());
         return replace('/login');
