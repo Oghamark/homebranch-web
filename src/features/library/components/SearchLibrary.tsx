45ca1da476d6ae5975a2b8ff48508e9bb3ea481c
Ryan Bezold <ryan.bezold@gmail.com>
2026-03-14 15:53:04 -0400

    fix: Don't clear input when mobile drawer closes
    Closes #61

--- src/features/library/components/SearchLibrary.tsx
+++ src/features/library/components/SearchLibrary.tsx
@@ -1,14 +1,15 @@
 import TextField from "@/components/ui/TextField";
-import {useAppDispatch} from "@/app/hooks";
+import {useAppDispatch, useAppSelector} from "@/app/hooks";
 import React from "react";
 import {updateQuery} from "@/features/library/store/librarySlice";
 
 export function SearchLibrary() {
     const dispatch = useAppDispatch();
+    const query = useAppSelector(state => state.library.query);
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         dispatch(updateQuery(e.currentTarget.value));
     }
 
-    return <TextField placeholder={"Search"} onChange={handleChange}/>
+    return <TextField placeholder={"Search"} value={query} onChange={handleChange}/>
 }
